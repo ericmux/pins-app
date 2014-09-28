b1904466c1ebf270b5bcb10e306e7fed0af21c4d
Marcio Paiva <marcioapf@gmail.com>
2014-09-27 20:52:50 -0400

    Login activity

---
+++ android/src/com/brh/pin/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.brh.pin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.login);
+    }
+}
